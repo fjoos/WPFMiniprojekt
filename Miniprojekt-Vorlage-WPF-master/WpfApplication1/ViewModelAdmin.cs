1609e2a436bfa4808cd9dde24207f771854121d2
fjoos <fabjoos@gmail.com>
2016-12-12 14:01:24 +0100

    vm

---
+++ Miniprojekt-Vorlage-WPF-master/WpfApplication1/ViewModelAdmin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gadgeothek.ViewModel
+{
+    public abstract class ViewModelAdmin : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
